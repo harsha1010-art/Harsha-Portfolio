3df67b23dfe32f88db69462fc1a31b36e990fb97
Harsha Srinivas <64862939+harsha1010-art@users.noreply.github.com>
2023-06-05 13:29:08 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,7 +17,7 @@ export const Contact = () => {
         "service_3tztab4",
         "template_k067u4h",
         form.current,
-        "IgNorAmfeGYxJiLjX"
+        "GhGeu74o-Lw97IxSb"
       )
       .then(
         (result) => {
